c7ac4d871d15690f2989703d94668e2543eeac3f
Ilaria Refaat <ilariarefaa20@gmail.com>
2022-09-06 16:21:23 +0200

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { NavigationBar } from "./Components/NavigationBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Dashboard } from "./Pages/Dashboard";
 import { LocationPage } from "./Pages/LocationPage";
 import { SearchPage } from "./Pages/SearchPage";
@@ -12,14 +12,14 @@ function App() {
     <Suspense fallback={null}>
       <div>
         <NavigationBar />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/location/:id" element={<LocationPage />} />
             <Route path="/activity/:id" element={<ActivityPage />} />
             <Route path="/Search" element={<SearchPage />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </Suspense>
   );
