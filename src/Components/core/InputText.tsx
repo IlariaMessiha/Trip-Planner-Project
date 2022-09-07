109b9d72be9548919a0d597e8c3a7b3bf88d11df
Ilaria Refaat <ilariarefaa20@gmail.com>
2022-09-07 14:01:44 +0200

    Material UI Input Text Dashboard

--- src/Components/core/InputText.tsx
+++ src/Components/core/InputText.tsx
@@ -1,22 +1,48 @@
+import { Box } from "@mui/material";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
 import { ChangeEventHandler, FC } from "react";
 import { FaSearch } from "react-icons/fa";
 import styles from "./InputText.module.css";
+import { styled } from "@mui/material/styles";
 interface InputTextProps {
   onChange: ChangeEventHandler<HTMLInputElement> | undefined;
   inputValue: string;
 }
+const SearchTextField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "black",
+
+    fontSize: "Larger",
+  },
+
+  "& .MuiFilledInput-root": {
+    border: "0px",
+    backgroundColor: "white",
+
+    "&:hover ": {
+      backgroundColor: "white",
+    },
+
+    "&.Mui-focused ": {
+      border: "0px",
+      backgroundColor: "white",
+    },
+  },
+});
 export const InputText: FC<InputTextProps> = ({ onChange, inputValue }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.searchIcon}>
-        <FaSearch />
-      </div>
-      <input
-        type="search"
+    <Box className={styles.container} sx={{ width: 600 }}>
+      <SearchTextField
+        label="Search For"
         className={styles.searchBar}
+        fullWidth
         value={inputValue}
         onChange={onChange}
+        variant="filled"
+        InputProps={{
+          disableUnderline: true,
+        }}
       />
-    </div>
+    </Box>
   );
 };
