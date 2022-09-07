dd96243acf9dd439f2f1651172f7278be8ef82eb
Ilaria Refaat <ilariarefaa20@gmail.com>
2022-09-07 11:38:23 +0200

    Material UI Card

--- src/Components/core/CardActivity.tsx
+++ src/Components/core/CardActivity.tsx
@@ -1,3 +1,4 @@
+import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { Activity } from "../../models/Activity";
@@ -11,12 +12,22 @@ interface CardActivityProps {
 export const CardActivity: FC<CardActivityProps> = ({ activity }) => {
   const { t } = useTranslation();
   return (
-    <div key={activity.id} className={styles.item}>
-      <img src={activity.coverImage} alt="activity" className={styles.image} />
-      <Typography text={activity.name} variant="h4" />
-      <Typography text={activity.location.name} />
-      <Typography text={activity.numberOfReviews} variant="body2" />
-      <Typography text={t("common.reviews")} variant="body2" />
-    </div>
+    <Card className={styles.item} sx={{ width: 320, height: 320 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={activity.coverImage}
+          alt="Activity Cover"
+          className={styles.image}
+          sx={{ height: 220 }}
+        />
+        <CardContent className={styles.ActivityContent}>
+          <Typography text={activity.name} variant="h4" />
+          <Typography text={activity.location.name} />
+          <Typography text={activity.numberOfReviews} variant="body2" />
+          <Typography text={t("common.reviews")} variant="body2" />
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
--- src/Components/core/CardLocation.tsx
+++ src/Components/core/CardLocation.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { CardActionArea } from "@mui/material";
 import { Location } from "../../models/Location";
 import { Typography } from "../core/Typography";
 import styles from "./CardLocation.module.css";
@@ -12,12 +15,22 @@ interface CardLocationProps {
 export const CardLocation: FC<CardLocationProps> = ({ location }) => {
   const { t } = useTranslation();
   return (
-    <div key={location.id} className={styles.item}>
-      <img src={location.coverImage} alt="Cover" className={styles.image} />
-      <Typography text={location.name} variant="h4" />
-      <Typography text={location.country} />
-      <Typography text={location.activities} variant="body2" />
-      <Typography text={t("common.activities")} variant="body2" />
-    </div>
+    <Card className={styles.item} sx={{ width: 320, height: 320 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={location.coverImage}
+          alt="Location Cover"
+          className={styles.image}
+          sx={{ height: 220 }}
+        />
+        <CardContent className={styles.locationContent}>
+          <Typography text={location.name} variant="h4" />
+          <Typography text={location.country} />
+          <Typography text={location.activities} variant="body2" />
+          <Typography text={t("common.activities")} variant="body2" />
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
