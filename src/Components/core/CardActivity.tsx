8f941f9067c8d954d713e5282ab8864e6e53c790
Ilaria Refaat <ilariarefaa20@gmail.com>
2022-09-12 10:46:44 +0200

    Favorite button

--- src/Components/core/CardActivity.tsx
+++ src/Components/core/CardActivity.tsx
@@ -1,33 +1,52 @@
-import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
+import styles from "./CardActivity.module.css";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  IconButton,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { Activity } from "../../models/Activity";
 import { Typography } from "../core/Typography";
-import styles from "./CardActivity.module.css";
 
 interface CardActivityProps {
   activity: Activity;
 }
-
+const FavoriteButton = styled(IconButton)({
+  position: "absolute",
+  backgroundColor: "white",
+  top: 4,
+  right: 10,
+});
 export const CardActivity: FC<CardActivityProps> = ({ activity }) => {
   const { t } = useTranslation();
   return (
-    <Card className={styles.item} sx={{ width: 320, height: 320 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          image={activity.coverImage}
-          alt="Activity Cover"
-          className={styles.image}
-          sx={{ height: 220 }}
-        />
-        <CardContent className={styles.ActivityContent}>
-          <Typography text={activity.name} variant="h4" />
-          <Typography text={activity.location.name} />
-          <Typography text={activity.numberOfReviews} variant="body2" />
-          <Typography text={t("common.reviews")} variant="body2" />
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div className={styles.container}>
+      <Card className={styles.item} sx={{ width: 320, height: 320 }}>
+        <CardActionArea>
+          <FavoriteButton>
+            <FavoriteIcon />
+          </FavoriteButton>
+          <CardMedia
+            component="img"
+            image={activity.coverImage}
+            alt="Activity Cover"
+            className={styles.image}
+            sx={{ height: 220 }}
+          />
+
+          <CardContent className={styles.ActivityContent}>
+            <Typography text={activity.name} variant="h4" />
+            <Typography text={activity.location.name} />
+            <Typography text={activity.numberOfReviews} variant="body2" />
+            <Typography text={t("common.reviews")} variant="body2" />
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
   );
 };
--- src/Components/core/CardLocation.tsx
+++ src/Components/core/CardLocation.tsx
@@ -3,20 +3,30 @@ import { useTranslation } from "react-i18next";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import { CardActionArea } from "@mui/material";
+import { CardActionArea, IconButton, styled } from "@mui/material";
 import { Location } from "../../models/Location";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { Typography } from "../core/Typography";
 import styles from "./CardLocation.module.css";
 
 interface CardLocationProps {
   location: Location;
 }
+const FavoriteButton = styled(IconButton)({
+  position: "absolute",
+  backgroundColor: "white",
+  top: 4,
+  right: 10,
+});
 
 export const CardLocation: FC<CardLocationProps> = ({ location }) => {
   const { t } = useTranslation();
   return (
     <Card className={styles.item} sx={{ width: 320, height: 320 }}>
       <CardActionArea>
+        <FavoriteButton>
+          <FavoriteIcon />
+        </FavoriteButton>
         <CardMedia
           component="img"
           image={location.coverImage}
