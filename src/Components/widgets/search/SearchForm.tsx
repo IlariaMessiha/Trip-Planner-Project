f54b7544b338a76cacb51d7ede04729ebf073eb9
Bichoy Messiha <bichoy.messiha@indigen.com>
2022-09-13 23:35:03 +0200

    fix search form

--- src/Components/widgets/search/SearchForm.tsx
+++ src/Components/widgets/search/SearchForm.tsx
@@ -1,50 +1,43 @@
 import { Autocomplete, Button, TextField } from "@mui/material";
-import React, { ChangeEvent, useState } from "react";
+import { FC, useState } from "react";
 
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
 import { apiCalls } from "../../../api/api";
-import { SearchResult, SearchResultType } from "../../../types/Search";
+import {
+  SearchQuery,
+  SearchResult,
+  SearchResultType,
+} from "../../../types/Search";
 import { InputText } from "../../core/InputText";
 import styles from "./SearchForm.module.css";
 
 type TypeOption = { id: SearchResultType; label: string };
 
-export const SearchForm = () => {
+interface SearchFormProps {
+  initialLabel: string;
+  onSubmit: (results: SearchResult[], query: SearchQuery) => void;
+}
+
+export const SearchForm: FC<SearchFormProps> = ({ initialLabel, onSubmit }) => {
   const { t } = useTranslation();
 
-  const [results, setResults] = useState<SearchResult[]>([]);
-  const [label, setLabel] = useState<string>("");
+  const [label, setLabel] = useState<string>(initialLabel);
   const [typeOption, setTypeOption] = useState<TypeOption | null>();
 
   const typeOptions: TypeOption[] = [
     { id: "location", label: t("common.locations") },
     { id: "activity", label: t("common.activities") },
   ];
-  const params = new URLSearchParams(window.location.search);
-  const searchWord = params.get("q");
-  const navigate = useNavigate();
 
-  React.useEffect(() => {
-    if (searchWord) {
-      setLabel(searchWord);
-      const _results = apiCalls.search({
-        label: searchWord,
-      });
-      setResults(_results);
-    }
-  }, [searchWord]);
-
-  const onSubmit = (e: any): void => {
+  const handleSubmit = (e: any): void => {
     e.preventDefault();
 
-    navigate(`/Search?q=${label}`);
-    const _results = apiCalls.search({ label, type: typeOption?.id });
-    setResults(_results);
+    const results = apiCalls.search({ label, type: typeOption?.id });
+    onSubmit(results, { label, type: typeOption?.id });
   };
 
   return (
-    <form className={styles.searchContainer} onSubmit={onSubmit}>
+    <form className={styles.searchContainer} onSubmit={handleSubmit}>
       <div className={styles.searchInputWrapper}>
         <InputText
           label={t("common.search")}
--- src/Pages/SearchPage.tsx
+++ src/Pages/SearchPage.tsx
@@ -1,11 +1,8 @@
-import { Autocomplete, TextField } from "@mui/material";
-import React, { ChangeEvent, useState } from "react";
-
-import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { apiCalls } from "../api/api";
+
 import { ActivitySearchResult } from "../Components/core/ActivitySearchResult";
-import { InputText } from "../Components/core/InputText";
 import { Container } from "../Components/core/layout/Container";
 import { LocationSearchResult } from "../Components/core/LocationSearchResult";
 import { SearchForm } from "../Components/widgets/search/SearchForm";
@@ -15,93 +12,25 @@ import { SearchResult } from "../types/Search";
 import styles from "./SearchPage.module.css";
 
 export const SearchPage = () => {
-  const { t } = useTranslation();
-  const options = [t("common.activities"), t("common.locations")];
   const [results, setResults] = useState<SearchResult[]>([]);
-  const [query, setQuery] = useState<string | undefined>("");
-  const [filter, setFilter] = useState<string | null>(null);
-  const params = new URLSearchParams(window.location.search);
-  const searchWord = params.get("q");
   const navigate = useNavigate();
-  const filteredResults: SearchResult[] = [];
-
-  // React.useEffect(() => {
-  //   if (searchWord) {
-  //     setQuery(searchWord);
-  //     const _results = apiCalls.search(searchWord);
-  //     setResults(_results);
-  //   }
-  // }, [searchWord]);
-
-  // const handleSearch = ({ target }: ChangeEvent<HTMLInputElement>): void => {
-  //   const _query: string = target.value.toLowerCase();
-  //   setQuery(_query);
-  // };
-
-  // const onSubmit = (e: any): void => {
-  //   e.preventDefault();
-
-  //   if (query) {
-  //     navigate(`/Search?q=${query}`);
-  //     const _results = apiCalls.search(query);
-
-  //     if (query === "") {
-  //       setResults([]);
-  //     } else {
-  //       if (filter === "Locations" || filter === "Ubicaciones") {
-  //         console.log("hi");
-  //         _results.map(({ item, type }) => {
-  //           if (type === "location") {
-  //             filteredResults.push({
-  //               type: "location",
-  //               item: item,
-  //             });
-  //           }
-  //           return filteredResults;
-  //         });
-  //         setResults(filteredResults);
-  //       } else if (
-  //         filter === "Activities" ||
-  //         filter === "Actividades" ||
-  //         filter === "Activités"
-  //       ) {
-  //         _results.map(({ type, item }) => {
-  //           if (type === "activity") {
-  //             filteredResults.push({
-  //               type: "activity",
-  //               item: item,
-  //             });
-  //           }
-  //           return filteredResults;
-  //         });
-  //         setResults(filteredResults);
-  //       } else {
-  //         setResults(_results);
-  //       }
-  //     }
-  //   }
-  // };
-
+  const { initialSearchLabel } = useInitialSearchFromUrl();
+  useEffect(() => {
+    const _results = apiCalls.search({
+      label: initialSearchLabel,
+    });
+    setResults(_results);
+  }, [initialSearchLabel]);
   return (
     <>
       <Container>
-        <SearchForm />
-        {/* <form className={styles.searchContainer} onSubmit={onSubmit}>
-          <InputTextSearchPage onChange={handleSearch} inputValue={query} />
-          <Autocomplete
-            value={filter}
-            disablePortal
-            options={options}
-            renderInput={(params) => (
-              <TextField {...params} label={t("common.filterBy")} />
-            )}
-            onChange={(event: any, newFilter: string | null) => {
-              setFilter(newFilter);
-            }}
-            sx={{ width: 300 }}
-          />
-          <input type="submit" className={styles.searchButton} value="Search" />
-        </form> */}
+        <SearchForm
+          initialLabel={initialSearchLabel}
+          onSubmit={(results, query) => {
+            setResults(results);
+            navigate(`/Search?q=${query.label}`);
+          }}
+        />
       </Container>
       <div className={styles.searchResultContainer}>
         <Container className={styles.searchResult}>
@@ -126,4 +55,18 @@ export const SearchPage = () => {
       </div>
     </>
   );
+};
+
+const useInitialSearchFromUrl = () => {
+  const [searchWord, setSearchWord] = useState("");
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const searchWord = params.get("q");
+    if (searchWord) {
+      setSearchWord(searchWord);
+    }
+  }, []);
+
+  return { initialSearchLabel: searchWord };
 };
