8997ad12da99c5304f5e2f7de45d3a44bf39fb7a
Ilaria Refaat <ilariarefaa20@gmail.com>
2023-06-12 18:30:37 +0300

    test cors url

--- Api/src/main.ts
+++ Api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         cors: {
-            origin: ["http://localhost:3000", "https://trip-planner-project.up.railway.app/"],
+            origin: ["http://localhost:3000", "https://trip-planner-project.up.railway.app"],
         },
     });
     const port = process.env.PORT || 3333;
