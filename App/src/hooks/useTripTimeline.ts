c91f661cdab499988a8c95c0edb24877ed4aa7f4
Bichoy Messiha <bichoy.messiha@indigen.com>
2023-06-16 20:42:54 +0300

    chore: remove warning

--- App/src/hooks/useTripTimeline.ts
+++ App/src/hooks/useTripTimeline.ts
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 import { TripDto } from "../types/dto/common/TripDto";
 import { mockTrip } from "../mock";
 
-const getTripFromLocalStorage = (key: string, defaultValue = {}) => {
-    try {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : defaultValue;
-    } catch (error) {
-        console.log(error);
-        return defaultValue;
-    }
-};
+// const getTripFromLocalStorage = (key: string, defaultValue = {}) => {
+//     try {
+//         const item = window.localStorage.getItem(key);
+//         return item ? JSON.parse(item) : defaultValue;
+//     } catch (error) {
+//         console.log(error);
+//         return defaultValue;
+//     }
+// };
 
 export const useTripTimeline = () => {
     const [trip, setTrip] = useState<TripDto | null>(null);
