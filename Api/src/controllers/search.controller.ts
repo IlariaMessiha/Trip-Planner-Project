2b029c4ef7c80900b0f706fae41064931a4973ab
Joseph <Josephdaoud@icloud.com>
2023-06-19 06:13:43 +0300

    removing unused imports

--- Api/src/controllers/search.controller.ts
+++ Api/src/controllers/search.controller.ts
@@ -1,8 +1,7 @@
 import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { SearchService } from "src/services/search.service";
-import { SearchQuery } from "src/types/dto/search/searchDto";
-import { filter } from "lodash";
+
 
 @Controller("")
 export class SearchController {
