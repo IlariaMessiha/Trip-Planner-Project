77a36ab6193d0a3035da6709b99726d664fa7551
Ilaria Refaat <ilariarefaa20@gmail.com>
2023-06-19 12:06:45 +0300

    using process.env in autocomplete

--- App/src/api/autoCompleteGet.ts
+++ App/src/api/autoCompleteGet.ts
@@ -1,14 +1,13 @@
 import Axios from "axios";
 import { AutoCompleteResult } from "../types/dto/autocompleteType";
-const API_BASE_URL = "http://localhost:3333";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export class AutoCompleteGet {
-
     public async autoCompleteSearch(searchQuery: string) {
-        console.log("auto complete query: ", searchQuery);
-        const res: any = await Axios.get<AutoCompleteResult[]>(`${API_BASE_URL}/autocomp/${searchQuery}`);
-        console.log("auto complete result: ", res);
-        return (await Axios.get<AutoCompleteResult[]>(`${API_BASE_URL}/autocomp/${searchQuery}`)).data;
+        const res: any = await Axios.get<AutoCompleteResult[]>(
+            `${API_BASE_URL}/autocomp/${searchQuery}`
+        );
+        return res.data;
     }
 }
 export const autoCompleteData = new AutoCompleteGet();
